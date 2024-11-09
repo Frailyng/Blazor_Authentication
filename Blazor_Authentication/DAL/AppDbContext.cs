6e8787f8a8ea8d4701f48c11da3daa845c7b6b96
frailyng <frailindes@gmail.com>
2024-11-09 13:41:40 -0400

    Appdbcontext creado

---
+++ Blazor_Authentication/DAL/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Blazor_Authentication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blazor_Authentication.DAL;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+
+    }
+
+    public DbSet<CuentaUsuario> CuentaUsuario { get; set; }
+}
