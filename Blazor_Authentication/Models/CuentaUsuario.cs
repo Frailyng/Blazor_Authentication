010f8544e37d66682b468954aedcbd5350f7f9b7
frailyng <frailindes@gmail.com>
2024-11-09 13:27:49 -0400

    Entidad CuentaUsuario Creada

---
+++ Blazor_Authentication/Models/CuentaUsuario.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blazor_Authentication.Models;
+
+[Table("user_account")]
+public class CuentaUsuario
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+
+    public int Id { get; set; }
+
+    [Column("nombre_usuario")]
+    [MaxLength(100)]
+
+    public string? NombreUsuario { get; set; }
+
+    [Column("contraseña")]
+    [MaxLength(100)]
+
+    public string? Contraseña { get; set; }
+
+    [Column("rol")]
+    [MaxLength(20)]
+
+    public string? Rol {  get; set; }
+}
